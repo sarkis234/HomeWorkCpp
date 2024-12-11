e07a07f5f9cc9b213c19efd58411c56fb6299d13
sarkis234 <sarkis95283@gmail.com>
2024-12-11 21:37:13 +0300

    Add files via upload

---
+++ HomeWork29/HomeWork29.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+#include "List.h";
+template <typename T>
+void operator<<(ostream& out, List<T>& list) {
+	list.print();
+}
+int main()
+{
+	List<int> list;
+	list.pushBack(4);
+	list.pushBack(1);
+	list.pushBack(2);
+	list.pushBack(5);
+	list.print();
+
+	/*cout << list[1] << endl;
+	int* arr = list();
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}*/
+	cout << endl;
+	list.pushIndex(3, 4);
+	list.print();
+	list.sort();
+	list.print();
+	cout << list;
+}
---
+++ HomeWork29/List.h
@@ -0,0 +1,154 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template <typename T>
+struct Node {
+	T value;
+	Node* next;
+
+	Node(T value) : value(value), next(nullptr) {}
+};
+
+template <typename T>
+class List
+{
+private:
+	Node<T>* first;
+public:
+	List() {
+		this->first = nullptr;
+	}
+	List(T* arr) {}
+	void pushBack(T value) {
+		if (this->empty()) {
+			this->first = new Node<T>(value);
+			return;
+		}
+		Node<T>* p = this->first;
+		while (p->next != nullptr) {
+			p = p->next;
+		}
+		p->next = new Node<T>(value);
+	}
+	void pushStart(T value) {
+		if (this->empty()) {
+			this->first = new Node<T>(value);
+			return;
+		}
+		Node<T>* p = this->first;
+		this->first = new Node<T>(value);
+		this->first->next = p;
+	}
+	void pushIndex(T value, int index) {
+		if (index >= this->getSize()) {
+			pushBack(value); // если индекс за рамками массива, элемент добавляется в конец
+		}
+		else if (index <= 0) {
+			pushStart(value);
+		}
+		else {
+			if (this->empty()) {
+				this->first = new Node<T>(value);
+				return;
+			}
+			Node<T>* p = this->first;
+			int counter = 0;
+			while (p ->next != nullptr && counter != index - 1) {
+				p = p->next;
+				counter++;
+			}
+			Node<T>* prev = p;
+			p = p->next;
+			Node<T>* p2 = p;
+			p = new Node<T>(value);
+			p->next = p2;
+			prev->next = p;
+		}	
+	}
+	void sort() {
+		bool flag;
+		Node<T>* p = this->first;
+		while (true) {
+			flag = true;
+			while (p->next != nullptr) {
+				if (p->value > p->next->value) {
+					swap(p->value, p->next->value);
+					flag = false;
+				}
+				p = p->next;
+			}
+			if (flag == true) {
+				break;
+			}
+			p = this->first;
+		}
+	}
+	void popBack() {
+		if (this->empty()) {
+			return;
+		}
+		Node<T>* p = this->first;
+		while (p->next->next != nullptr) {
+			p = p->next;
+		}
+		delete p->next;
+		p->next = nullptr;
+	}
+	void popStart() {
+		Node<T>* p = this->first->next;
+		delete this->first;
+		this->first = p;
+	}
+	Node<T>* search(T value) {
+		Node<T>* p = this->first;
+		while (p != nullptr) {
+			if (p->value == value) {
+				return p;
+			}
+			p = p->next;
+		}
+		return nullptr;
+	}
+	void print() {
+		Node<T>* p = this->first;
+		while (p != nullptr) {
+			cout << p->value << " ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+	T operator[](int ind) {
+		Node<T>* p = this->first;
+		for (int i = 0; i < ind; i++) {
+			p = p->next;
+		}
+		return p->value;
+	}
+	T* operator()() {
+		Node<T>* p = this->first;
+		int count = 0;
+		T* arr = new T[getSize()];
+		while (p != nullptr) {
+			arr[count++] = p->value;
+			p = p->next;
+		}
+		return arr;
+	}
+	int getSize() {
+		Node<T>* p = this->first;
+		int count = 0;
+		while (p != nullptr) {
+			p = p->next;
+			count++;
+		}
+		return count;
+	}
+	bool empty() { return (first == nullptr) ? true : false; }
+	void clearMemory() {
+		Node<T>* p = this->first;
+		while (p != nullptr) {
+			delete p;
+			p = p->next;
+		}
+	}
+};
